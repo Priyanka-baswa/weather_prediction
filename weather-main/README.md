1aaa1bb39b4807ecd701428c81fe1b33ed8ad9a3
Baswa Priyanka <135412862+Priyanka-baswa@users.noreply.github.com>
2023-06-28 15:09:12 +0530

    Add files via upload

---
+++ weather-main/README.md
@@ -0,0 +1,13 @@
+
+
+
+
+[Weather APP]) is developed using React.js where User can search their city name and observe their weather for the next 5-6 days.
+
+
+
+# Used libraries
+
+- `react-js`
+- `material-ui`
+
---
+++ weather-main/src/api/OpenWeatherService.js
@@ -0,0 +1,47 @@
+const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+const WEATHER_API_KEY = 'f0ea19bd75f15e40ad09f16011f4f4f3';
+
+const GEO_API_OPTIONS = 
+{
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ec5fa3c475msha91574127f436dap193680jsn8157d4af32f4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export async function fetchWeatherData(lat, lon) {
+  try {
+    let [weatherPromise, forcastPromise] = await Promise.all([
+      fetch(
+        `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+      fetch(
+        `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+    ]);
+
+    const weatherResponse = await weatherPromise.json();
+    const forcastResponse = await forcastPromise.json();
+    return [weatherResponse, forcastResponse];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchCities(input) {
+  try {
+    const response = await fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${input}`,
+      GEO_API_OPTIONS
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
